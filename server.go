ab08d003aff6bf641ab1f0f7900204d96db10d5d
Peter P. Gengler <peter.gengler@jumpcloud.com>
2017-03-28 20:14:32 -0600

    Add RegisterServiceInfo to decouple Register from grpc.Server.

--- server.go
+++ server.go
@@ -10,11 +10,17 @@ import (
 	"google.golang.org/grpc"
 )
 
-// PreregisterServices takes a gRPC server and pre-initializes all counters to 0.
-// This allows for easier monitoring in Prometheus (no missing metrics), and should be called *after* all services have
-// been registered with the server.
+// Register takes a gRPC server and pre-initializes all counters to 0.
+// This allows for easier monitoring in Prometheus (no missing metrics), and
+// should be called *after* all services have been registered with the server.
 func Register(server *grpc.Server) {
-	serviceInfo := server.GetServiceInfo()
+	RegisterServiceInfo(server.GetServiceInfo())
+}
+
+// RegisterServiceInfo takes a map of gRPC ServiceInfo objects and per-initializes all counters to 0.
+// This allows for easier monitoring in Prometheus (no missing metrics), and
+// should be called *after* all services have been registered with the server.
+func RegisterServiceInfo(serviceInfo map[string]grpc.ServiceInfo) {
 	for serviceName, info := range serviceInfo {
 		for _, mInfo := range info.Methods {
 			preRegisterMethod(serviceName, &mInfo)
